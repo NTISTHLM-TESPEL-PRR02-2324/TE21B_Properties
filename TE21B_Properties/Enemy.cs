9c9f955af4e4afd5f01a63e27b333712f8afc8d5
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-11-13 20:10:32 +0100

    Initial commit

---
+++ TE21B_Properties/Enemy.cs
@@ -0,0 +1,53 @@
+﻿public class Enemy
+{
+  public string Name { get => name; set => name = value; }
+
+  private int _hp = 100;
+
+  public int Hp
+  {
+    get
+    {
+      Console.WriteLine("GET!");
+      return Random.Shared.Next(100);
+    }
+    set
+    {
+      _hp = value;
+      if (_hp < 0) _hp = 0;
+      if (_hp > 100) _hp = 100;
+    }
+  }
+
+  private int _xp;
+  private string name;
+
+  public int Level
+  {
+    get
+    {
+      return _xp / 10 + 1;
+    }
+    private set
+    {
+
+    }
+  }
+
+
+  public int GetHp()
+  {
+    return _hp;
+  }
+
+  public void SetHp(int value)
+  {
+    _hp = value;
+    if (_hp < 0) _hp = 0;
+  }
+
+  public void Hurt(int amount)
+  {
+    SetHp(GetHp() - amount);
+  }
+}
---
+++ TE21B_Properties/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Enemy e = new();
+
+// Console.WriteLine(e.Hp);
+
+int t = e.Hp;
+
+// e.Hp -= 50;
+// e.Hp -= 50;
+// e.Hp -= 50;
+
+
+
+Console.ReadLine();
